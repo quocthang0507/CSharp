edac9de0b086c0fc38692d628e701d88b0ae3a83
La Quốc Thắng <quocthang0507@gmail.com>
2020-02-28 17:15:21 +0700

    Upload new project

---
+++ ReadingDigit/ReadingDigit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace ReadingDigit
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			DocSoTiengViet soTiengViet = new DocSoTiengViet();
+			Console.WriteLine(soTiengViet.DocTienBangChu(20202, ""));
+			Console.ReadKey();
+		}
+	}
+}
