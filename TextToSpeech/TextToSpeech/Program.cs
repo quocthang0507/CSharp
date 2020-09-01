875dd5e13bd0e44f25c043d4058156105a97ffd4
La Quốc Thắng <quocthang0507@gmail.com>
2020-09-01 22:18:43 +0700

    Update project

--- TextToSpeech/TextToSpeech/Program.cs
+++ TextToSpeech/TextToSpeech/Program.cs
@@ -9,14 +9,49 @@ namespace TextToSpeech
 {
 	class Program
 	{
+		private const string terminal = "Type your English sentence ('exit' to exit): ";
+		private const string introduction = "Welcome to Text To Speech Console Application";
+		private const string goodbye = "Good bye!";
+
 		static void Main(string[] args)
 		{
+			Program2.Main2(args);
+		}
+
+		public static void Main1(string[] args)
+		{
+			// Initialize a new instance of the SpeechSynthesizer.  
 			using (SpeechSynthesizer synth = new SpeechSynthesizer())
 			{
-				synth.SetOutputToDefaultAudioDevice();
-				synth.Speak("Welcome to TOEIC Mastery");
+				SetUpSpeech(synth);
+				Console.WriteLine(introduction);
+				synth.Speak(introduction);
+				while (true)
+				{
+					Console.Write(terminal);
+					synth.Speak(terminal);
+					string text = Console.ReadLine();
+					if (text == "exit")
+					{
+						Console.WriteLine(goodbye);
+						synth.Speak(goodbye);
+						break;
+					}
+					synth.Speak(text);
+				}
 			}
 			Console.ReadKey();
+		}
+
+		static void SetUpSpeech(SpeechSynthesizer synth)
+		{
+			// Configure the audio output
+			synth.SetOutputToDefaultAudioDevice();
+			Console.WriteLine("What gender would you like to speak?");
+			Console.WriteLine("1. Male\n2. Female\n3. Neutral");
+			int voiceIndex = Convert.ToInt32(Console.ReadLine());
+			if (voiceIndex == 1 || voiceIndex == 2 || voiceIndex == 3)
+				synth.SelectVoiceByHints((VoiceGender)voiceIndex);
 		}
 	}
 }
---
+++ TextToSpeech/TextToSpeech/Program2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToSpeech
+{
+	public class Program2
+	{
+		public static void Main2(string[] args)
+		{
+
+			// Initialize a new instance of the SpeechSynthesizer.  
+			using (SpeechSynthesizer synth = new SpeechSynthesizer())
+			{
+
+				// Get information about supported audio formats.  
+				string AudioFormats = "";
+				foreach (SpeechAudioFormatInfo fmt in synth.Voice.SupportedAudioFormats)
+				{
+					AudioFormats += String.Format("{0}\n",
+					fmt.EncodingFormat.ToString());
+				}
+
+				// Write information about the voice to the console.  
+				Console.WriteLine(" Name:          " + synth.Voice.Name);
+				Console.WriteLine(" Culture:       " + synth.Voice.Culture);
+				Console.WriteLine(" Age:           " + synth.Voice.Age);
+				Console.WriteLine(" Gender:        " + synth.Voice.Gender);
+				Console.WriteLine(" Description:   " + synth.Voice.Description);
+				Console.WriteLine(" ID:            " + synth.Voice.Id);
+				if (synth.Voice.SupportedAudioFormats.Count != 0)
+				{
+					Console.WriteLine(" Audio formats: " + AudioFormats);
+				}
+				else
+				{
+					Console.WriteLine(" No supported audio formats found");
+				}
+
+				// Get additional information about the voice.  
+				string AdditionalInfo = "";
+				foreach (string key in synth.Voice.AdditionalInfo.Keys)
+				{
+					AdditionalInfo += String.Format("  {0}: {1}\n",
+					  key, synth.Voice.AdditionalInfo[key]);
+				}
+
+				Console.WriteLine(" Additional Info - " + AdditionalInfo);
+				Console.WriteLine();
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+}
